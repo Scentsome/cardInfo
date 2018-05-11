7cea753554e7f0c92a52ffd73c1099e5f21d67ed
Michael Pan <scentsome@gmail.com>
2018-05-11 11:49:59 +0800

    Migrate to Postgres DB

--- Sources/App/Models/Acrynom.swift
+++ Sources/App/Models/Acrynom.swift
@@ -1,8 +1,18 @@
-//
-//  Acrynom.swift
-//  App
-//
-//  Created by Michael on 2018/5/11.
-//
+import Vapor
+import FluentPostgreSQL
 
-import Foundation
+final class Acronym: Codable {
+    var id: Int?
+    var short: String
+    var long: String
+    
+    init(short: String, long: String) {
+        self.short = short
+        self.long = long
+    }
+}
+
+extension Acronym: PostgreSQLModel {}
+extension Acronym: Migration {}
+extension Acronym: Content {}
+
